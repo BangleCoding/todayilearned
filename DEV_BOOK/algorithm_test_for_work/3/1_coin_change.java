7edf0cb3ef3222cee6d3c14c1f28e627994fe4ed
BangleCoder <47234375+BangleCoding@users.noreply.github.com>
2021-05-10 17:22:21 +0900

    Update 1_coin_change.java

--- DEV_BOOK/algorithm_test_for_work/3/1_coin_change.java
+++ DEV_BOOK/algorithm_test_for_work/3/1_coin_change.java
@@ -1,5 +1,15 @@
-
-public class Main{
-
+public class CoinChange {
+    public static void main(String args[]) {
+      int change = 1260; 
+      int cnt = 0; 
+      int[] coinTypes = {500, 100, 50, 10};
+      
+      for(int i=0; i< coinTypes.length; i++){
+          int coin = coinTypes[i]; 
+          cnt += change /coin; 
+          change %=coin; 
+         }
+      
+    System.out.println(cnt); 
+    }
 }
-
