94ca4c92b389933a65bb73589e1ee384b2f65004
BangleCoder <47234375+BangleCoding@users.noreply.github.com>
2021-07-25 20:58:01 +0900

    Create 018_ForTest1Main.java

---
+++ JAVA/JAVA200/018_ForTest1Main.java
@@ -0,0 +1,19 @@
+
+public class ForTest1Main {
+	
+	public static void main(String[] args) {
+		for(int j=40; j<50; j++) {
+			System.out.printf("%d ", j);
+		}
+		
+		System.out.printf("%n");
+		System.out.println();
+		
+		int i =0;
+		for(; i<9;i++) {
+			System.out.print(i+"");
+		}
+		System.out.println("\ni의 값은:"+i);
+	}
+
+}
